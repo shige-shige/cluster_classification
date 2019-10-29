8d81594551a7ac5a398404e92ac355ffa8191a15
shige13 <watanabe@atsu-maru.co.jp>
2019-10-29 23:11:52 +0900

    feat: リセット機能追加

--- webserver.py
+++ webserver.py
@@ -1,11 +1,13 @@
 import os
-from flask import Flask, request, redirect, url_for, flash
-from werkzeug.utils import secure_filename
 
+from flask import flash, Flask, redirect, render_template, request, url_for
+from flask import send_from_directory
 from keras.models import Sequential, load_model
+from PIL import Image
+from werkzeug.utils import secure_filename
 import keras,sys
 import numpy as np
-from PIL import Image
+
 
 CLASSES = ['MORE', 'Ray', 'VERY']
 NUM_CLASSES = len(CLASSES)
@@ -15,6 +17,7 @@ IMAGE_SIZE_H = 100
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -22,22 +25,22 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def upload_file():  
     if request.method == 'POST':
+        print('============>POST開始')
         if 'file' not in request.files:
             flash('ファイルがありません')
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
-            flash('ファイルがありません')
+            flash('ファイル名がありません')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
+            keras.backend.clear_session()
             model = load_model('./visual_cnn.h5')
-
             image = Image.open(filepath)
             image = image.convert('RGB')
             image = image.resize((IMAGE_SIZE_W, IMAGE_SIZE_H))
@@ -45,35 +48,24 @@ def upload_file():
             X = []
             X.append(data)
             X = np.array(X)
-
             result = model.predict([X])[0]
             predicted = result.argmax()
             percentage = int(result[predicted] * 100)
 
-            return "ラベル： " + CLASSES[predicted] + ", 確率："+ str(percentage) + " %"
-            #return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <title>ファイルをアップロードして判定しよう</title></head>
-    <body>
-    <h1>ファイルをアップロードして判定しよう！</h1>
-    <form method = post enctype = multipart/form-data>
-    <p><input type=file name=file>
-    <input type=submit value=Upload>
-    </form>
-    </body>
-    </html>
-    '''
+            os.remove('./uploads/' + filename)
 
-from flask import send_from_directory
+            return render_template('index.html', filename=filename, predicted=CLASSES[predicted], percentage=percentage)
+    return render_template('index.html')
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/reset')
+def reset_site():
+    return redirect(url_for('upload_file'))
 
 def main():
     app.debug = True
